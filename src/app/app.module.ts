7863f68a3e1aaaf717d63e8952693d4646732cde
GiacomoMansi <giacomomansi@hotmail.com>
2022-08-24 11:34:27 +0200

    added a Search bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { HttpClientModule } from "@angular/common/http";
 import { AppComponent } from './app.component';
 import { PokemonSearchComponent } from './pages/pokemon-cards/pokemon-cards.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { Ng2SearchPipeModule } from 'ng2-search-filter'
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
@@ -13,6 +15,8 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     BrowserModule,
     HttpClientModule,
     NgbModule,
+    Ng2SearchPipeModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/pokemon-cards/pokemon-cards.component.ts
+++ src/app/pages/pokemon-cards/pokemon-cards.component.ts
@@ -4,34 +4,35 @@ import {HttpClient} from "@angular/common/http";
 
 
 
-
 @Component({
   selector: 'app-pokemon-cards',
   templateUrl: './pokemon-cards.html',
   styleUrls: ['./pokemon-cards.component.css']
 })
 export class PokemonSearchComponent implements OnInit {
-constructor(public pokemonService: PokemonService, public http: HttpClient,) { }
+
+  constructor(public pokemonService: PokemonService, public http: HttpClient,) { }
 public pokemonApi: any = [];
 public allPokemonList: any = [];
 public pokemon:  any = [];
+public selectBasicLoading = false;
+public item: any = []
 
-// TODO: Commentare tutto
 
   async ngOnInit() {
-    this.pokemonApi = await this.pokemonService.getPokemon()
-    this.allPokemonList = await this.pokemonApi.results
-    let listObjects: any = [];
+    this.pokemonApi = await this.pokemonService.getPokemon() //Chiamo il service
+    this.allPokemonList = await this.pokemonApi.results //Inserisco il service nell'array allPokemonList
+
     const pokemonCompleteList = async (): Promise<any> => {
       await Promise.all(
-        this.allPokemonList.map(async (pokemon: any): Promise<any> => {
+        this.allPokemonList.map(async (pokemon: any): Promise<any> => { //
           const aPokemon = await this.http.get(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`).toPromise()
           this.pokemon.push(aPokemon)
         })
       )
-      return listObjects;
     }
     await pokemonCompleteList()
     console.log(this.pokemon)
   }
+
 }
