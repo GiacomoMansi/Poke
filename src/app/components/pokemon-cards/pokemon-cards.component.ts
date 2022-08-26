f91251f458d6535f59bc6d1a143ffd6fa2ed0251
GiacomoMansi <giacomomansi@hotmail.com>
2022-08-26 15:58:24 +0200

    added an example of component without using pipes

--- src/app/components/pokemon-cards/pokemon-cards.component.ts
+++ src/app/components/pokemon-cards/pokemon-cards.component.ts
@@ -9,7 +9,6 @@ import {HttpClient} from "@angular/common/http";
   styleUrls: ['./pokemon-cards.component.css']
 })
 export class PokemonSearchComponent implements OnInit {
-
   constructor(public pokemonService: PokemonService, public http: HttpClient,) {
   }
 
@@ -17,7 +16,7 @@ export class PokemonSearchComponent implements OnInit {
   public pokemon: any = [];
   public onePokemon: any = [];
   public selectedOption: string = ""
-
+  public args: any = [];
   public options = [
     {name: "All", value: ""},
     {name: "Grass", value: "grass"},
@@ -63,6 +62,7 @@ export class PokemonSearchComponent implements OnInit {
     }
     await pokemonCompleteList()
 
+
   }
 
   //Search Input by Pokemon Name or Pokemon Type
@@ -90,5 +90,151 @@ export class PokemonSearchComponent implements OnInit {
     currentPage: 1,
     collection: this.pokemon.length,
   };
+
+
+  //Sorting by name, type, and statistics
+  public orderName: boolean = false;
+  public orderType: boolean = false;
+  public orderHp: boolean = false;
+  public orderAttack: boolean = false;
+  public orderDefense: boolean = false;
+  public orderSpecialAttack: boolean = false;
+  public orderSpecialDefense: boolean = false;
+  public orderSpeed: boolean = false;
+
+
+  sortPokemonName() {
+    let multiplier = 1;
+    if (!this.orderName) {
+      multiplier = -1
+    }
+    this.pokemon.sort((a: any, b: any,) => {
+      if (a.name < b.name) {
+        return -1 * multiplier
+      } else if (a.name > b.name) {
+        return multiplier;
+      } else {
+        return 0
+      }
+    })
+    this.orderName = !this.orderName
+  }
+
+  sortPokemonType() {
+    this.pokemon.sort((a: any, b: any,) => {
+      if (a.typo < b.typo) {
+        return -1
+      } else if (a.typo > b.typo) {
+        if (!this.orderType) {
+          return -1
+        }
+      }
+      return 0
+    })
+    this.orderType = !this.orderType
+  }
+
+  sortPokemonHp() {
+    let multiplier = 1;
+    if (!this.orderHp) {
+      multiplier = -1
+    }
+
+    this.pokemon.sort((a: any, b: any, name: string = "") => {
+      if (a.hp < b.hp) {
+        return -1 * multiplier
+      } else if (a.hp > b.hp) {
+        return multiplier;
+      } else {
+        return 0
+      }
+    })
+    this.orderHp = !this.orderHp
+  }
+
+  sortPokemonAttack() {
+    let multiplier = 1;
+    if (!this.orderAttack) {
+      multiplier = -1
+    }
+    this.pokemon.sort((a: any, b: any, name: string = "") => {
+      if (a.attack < b.attack) {
+        return -1 * multiplier
+      } else if (a.attack > b.attack) {
+        return multiplier;
+      } else {
+        return 0
+      }
+    })
+    this.orderAttack = !this.orderAttack
+  }
+
+  sortPokemonDefense() {
+    let multiplier = 1;
+    if (!this.orderDefense) {
+      multiplier = -1
+    }
+    this.pokemon.sort((a: any, b: any, name: string = "") => {
+      if (a.defense < b.defense) {
+        return -1 * multiplier
+      } else if (a.defense > b.defense) {
+        return multiplier;
+      } else {
+        return 0
+      }
+    })
+    this.orderDefense = !this.orderDefense
+  }
+
+  sortPokemonSpecialAttack() {
+    let multiplier = 1;
+    if (!this.orderSpecialAttack) {
+      multiplier = -1
+    }
+    this.pokemon.sort((a: any, b: any, name: string = "") => {
+      if (a.specialAttack < b.specialAttack) {
+        return -1 * multiplier
+      } else if (a.specialAttack > b.specialAttack) {
+        return multiplier;
+      } else {
+        return 0
+      }
+    })
+    this.orderSpecialAttack = !this.orderSpecialAttack
+  }
+
+  sortPokemonSpecialDefense() {
+    let multiplier = 1;
+    if (!this.orderSpecialDefense) {
+      multiplier = -1
+    }
+    this.pokemon.sort((a: any, b: any, name: string = "") => {
+      if (a.specialDefense < b.specialDefense) {
+        return -1 * multiplier
+      } else if (a.specialDefense > b.specialDefense) {
+        return multiplier;
+      } else {
+        return 0
+      }
+    })
+    this.orderSpecialDefense = !this.orderSpecialDefense
+  }
+
+  sortPokemonSpeed() {
+    let multiplier = 1;
+    if (!this.orderSpeed) {
+      multiplier = -1
+    }
+    this.pokemon.sort((a: any, b: any, name: string = "") => {
+      if (a.speed < b.speed) {
+        return -1 * multiplier
+      } else if (a.speed > b.speed) {
+        return multiplier;
+      } else {
+        return 0
+      }
+    })
+    this.orderSpeed = !this.orderSpeed
+  }
 }
 
