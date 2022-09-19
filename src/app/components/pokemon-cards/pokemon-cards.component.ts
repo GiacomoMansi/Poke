600025b544719dd59c11361bb702033f9d597785
GiacomoMansi <giacomomansi@hotmail.com>
2022-09-19 21:03:15 +0200

    Refactor code

--- src/app/components/pokemon-cards/pokemon-cards.component.ts
+++ src/app/components/pokemon-cards/pokemon-cards.component.ts
@@ -10,6 +10,7 @@ export interface Pokemon {
 
   map(pokemon: any): any;
 }
+
 export class PokemonApi {
   count: number = 0;
   next: string = "";
@@ -98,10 +99,9 @@ export class PokemonSearchComponent implements OnInit {
   key = "";
   reverse: boolean = false;
 
-  sort(key: any) {
+  sort(key: any,) {
     this.key = key
     this.reverse = !this.reverse
-    let reverseName = !this.reverse
   }
 
   //Configurazione paginazione
@@ -110,152 +110,6 @@ export class PokemonSearchComponent implements OnInit {
     currentPage: 1,
     collection: this.pokemon.length,
   };
-
-
-  //Sorting by name, type, and statistics
-  public orderName: boolean = false;
-  public orderType: boolean = false;
-  public orderHp: boolean = false;
-  public orderAttack: boolean = false;
-  public orderDefense: boolean = false;
-  public orderSpecialAttack: boolean = false;
-  public orderSpecialDefense: boolean = false;
-  public orderSpeed: boolean = false;
-
-  //Sorting fatto a mano per ogni singola voce(per capire meglio il funzionamento del pipe orderBy)
-  // sortPokemonName() {
-  //   let multiplier = 1;
-  //   if (!this.orderName) {
-  //     multiplier = -1
-  //   }
-  //   this.pokemon.sort((a: any, b: any,) => {
-  //     if (a.name < b.name) {
-  //       return -1 * multiplier
-  //     } else if (a.name > b.name) {
-  //       return multiplier;
-  //     } else {
-  //       return 0
-  //     }
-  //   })
-  //   this.orderName = !this.orderName
-  // }
-  //
-  // sortPokemonType() {
-  //   this.pokemon.sort((a: any, b: any,) => {
-  //     if (a.typo < b.typo) {
-  //       return -1
-  //     } else if (a.typo > b.typo) {
-  //       if (!this.orderType) {
-  //         return -1
-  //       }
-  //     }
-  //     return 0
-  //   })
-  //   this.orderType = !this.orderType
-  // }
-  //
-  // sortPokemonHp() {
-  //   let multiplier = 1;
-  //   if (!this.orderHp) {
-  //     multiplier = -1
-  //   }
-  //
-  //   this.pokemon.sort((a: any, b: any) => {
-  //     if (a.hp < b.hp) {
-  //       return -1 * multiplier
-  //     } else if (a.hp > b.hp) {
-  //       return multiplier;
-  //     } else {
-  //       return 0
-  //     }
-  //   })
-  //   this.orderHp = !this.orderHp
-  // }
-  //
-  // sortPokemonAttack() {
-  //   let multiplier = 1;
-  //   if (!this.orderAttack) {
-  //     multiplier = -1
-  //   }
-  //   this.pokemon.sort((a: any, b: any) => {
-  //     if (a.attack < b.attack) {
-  //       return -1 * multiplier
-  //     } else if (a.attack > b.attack) {
-  //       return multiplier;
-  //     } else {
-  //       return 0
-  //     }
-  //   })
-  //   this.orderAttack = !this.orderAttack
-  // }
-  //
-  // sortPokemonDefense() {
-  //   let multiplier = 1;
-  //   if (!this.orderDefense) {
-  //     multiplier = -1
-  //   }
-  //   this.pokemon.sort((a: any, b: any,) => {
-  //     if (a.defense < b.defense) {
-  //       return -1 * multiplier
-  //     } else if (a.defense > b.defense) {
-  //       return multiplier;
-  //     } else {
-  //       return 0
-  //     }
-  //   })
-  //   this.orderDefense = !this.orderDefense
-  // }
-  //
-  // sortPokemonSpecialAttack() {
-  //   let multiplier = 1;
-  //   if (!this.orderSpecialAttack) {
-  //     multiplier = -1
-  //   }
-  //   this.pokemon.sort((a: any, b: any) => {
-  //     if (a.specialAttack < b.specialAttack) {
-  //       return -1 * multiplier
-  //     } else if (a.specialAttack > b.specialAttack) {
-  //       return multiplier;
-  //     } else {
-  //       return 0
-  //     }
-  //   })
-  //   this.orderSpecialAttack = !this.orderSpecialAttack
-  // }
-  //
-  // sortPokemonSpecialDefense() {
-  //   let multiplier = 1;
-  //   if (!this.orderSpecialDefense) {
-  //     multiplier = -1
-  //   }
-  //   this.pokemon.sort((a: any, b: any) => {
-  //     if (a.specialDefense < b.specialDefense) {
-  //       return -1 * multiplier
-  //     } else if (a.specialDefense > b.specialDefense) {
-  //       return multiplier;
-  //     } else {
-  //       return 0
-  //     }
-  //   })
-  //   this.orderSpecialDefense = !this.orderSpecialDefense
-  // }
-  //
-  // sortPokemonSpeed() {
-  //   let multiplier = 1;
-  //   if (!this.orderSpeed) {
-  //     multiplier = -1
-  //   }
-  //   this.pokemon.sort((a: any, b: any) => {
-  //     if (a.speed < b.speed) {
-  //       return -1 * multiplier
-  //     } else if (a.speed > b.speed) {
-  //       return multiplier;
-  //     } else {
-  //       return 0
-  //     }
-  //   })
-  //   this.orderSpeed = !this.orderSpeed
-  // }
 
 
 }
