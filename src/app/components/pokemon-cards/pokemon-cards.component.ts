1593c4ca0278fcd8a7b9e074a97965e48f6b01a7
GiacomoMansi <giacomomansi@hotmail.com>
2022-10-18 20:48:22 +0200

    cancellato search pipe

--- src/app/components/pokemon-cards/pokemon-cards.component.ts
+++ src/app/components/pokemon-cards/pokemon-cards.component.ts
@@ -33,51 +33,15 @@ export class PokemonSearchComponent implements OnInit {
   constructor(public http: HttpClient, public _pokemonService: PokemonService) {
   }
 
-  public pokemonApi: any = []; // Array contenente la chiamata all'api
-  public pokemon: any = [];
-  public onePokemon: any = [];
-  public selectedOption: string = ""
-  public errormessage: string = "";
-  public isLoading: boolean = true
+
+
 
 
   async ngOnInit() {
-    await this._pokemonService.getPokemon()//Chiamo la funzione che è nel service per caricare i pokemon
+
   }
 
-  // async getPokemon() { //To Promise invertito in subscribe
-  //   this.http.get("https://pokeapi.co/api/v2/pokemon/?offset=0&limit=500").subscribe({
-  //     next: (result: any,) => {
-  //       this.pokemonApi = result.results
-  //       const pokemonCompleteList = async (): Promise<any> => {
-  //         await Promise.all(
-  //           this.pokemonApi.map(async (pokemon: any): Promise<any> => {
-  //             this.onePokemon = await firstValueFrom(this.http.get(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`))
-  //               .catch(err =>
-  //                 this.errormessage = err.message)//Gestisco gli errori
-  //             this.pokemon.push(this.onePokemon) //Inserisco il singolo pokemon nell'array pokemon
-  //             //Assegno le statistiche all'oggetto onePokemon
-  //             Object.assign(this.onePokemon, {hp: this.onePokemon.stats[0].base_stat})
-  //             Object.assign(this.onePokemon, {attack: this.onePokemon.stats[1].base_stat})
-  //             Object.assign(this.onePokemon, {defense: this.onePokemon.stats[2].base_stat})
-  //             Object.assign(this.onePokemon, {specialAttack: this.onePokemon.stats[3].base_stat})
-  //             Object.assign(this.onePokemon, {specialDefense: this.onePokemon.stats[4].base_stat})
-  //             Object.assign(this.onePokemon, {speed: this.onePokemon.stats[5].base_stat})
-  //             //Assegno il tipo di pokemon all'oggetto onePokemon
-  //             Object.assign(this.onePokemon, {typo: this.onePokemon.types[0].type.name})
-  //           })
-  //         )
-  //       }
-  //       this.isLoading = false;
-  //       pokemonCompleteList()
-  //     },
-  //     error: (err) => { //Gestisco gli errori del subscribe
-  //       console.error(err.message)
-  //       this.errormessage = err.message
-  //     },
-  //     complete: () => {}
-  //   })
-  // }
+
 
 
 
@@ -85,13 +49,11 @@ export class PokemonSearchComponent implements OnInit {
   config = {
     itemsPerPage: 30,
     currentPage: 1,
-    collection: this.pokemon.length,
+    collection: this._pokemonService.pokemon.length,
   };
 
 
-  ngOnDestroy() {
-    this.pokemonApi.unsubscribe();
-  }
+
 
 
 }
