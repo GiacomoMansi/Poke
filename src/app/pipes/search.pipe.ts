416ef7085f47c6c7df32bc84d693122b6b4190bf
GiacomoMansi <giacomomansi@hotmail.com>
2022-10-18 18:00:46 +0200

    aggiunta interface

--- src/app/pipes/search.pipe.ts
+++ src/app/pipes/search.pipe.ts
@@ -1,11 +1,12 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import {Pokemon} from "../components/pokemon-cards/pokemon-cards.component";
 
 @Pipe({
   name: 'search'
 })
 export class SearchPipe implements PipeTransform {
 
-  public transform(value: any, keys: string, term: string) {
+  public transform(value: Pokemon[], keys: string, term: string) {
     if (!term) return value;
     // @ts-ignore
     return (value || []).filter(item => keys.split(',').some(key => item.hasOwnProperty(key) && new RegExp(term, 'gi').test(item[key])));
