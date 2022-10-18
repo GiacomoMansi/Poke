4ed3653a911e0e7c3965d23266111923c1886984
GiacomoMansi <giacomomansi@hotmail.com>
2022-10-18 17:58:45 +0200

    Tolti object assign rimpiazzati con un singolo oggetto

--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {firstValueFrom} from "rxjs";
 import {HttpClient} from "@angular/common/http";
 
@@ -12,7 +12,9 @@ export class PokemonService {
   public selectedOption: string = ""
   public errormessage: string = "";
   public isLoading: boolean = true
-  constructor(public _httpClient: HttpClient) { }
+
+  constructor(public _httpClient: HttpClient) {
+  }
 
   async getPokemon() { //To Promise invertito in subscribe
     this._httpClient.get("https://pokeapi.co/api/v2/pokemon/?offset=0&limit=500").subscribe({
@@ -25,27 +27,37 @@ export class PokemonService {
                 .catch(err =>
                   this.errormessage = err.message)//Gestisco gli errori
               this.pokemon.push(this.onePokemon) //Inserisco il singolo pokemon nell'array pokemon
-              //Assegno le statistiche all'oggetto onePokemon
-              Object.assign(this.onePokemon, {hp: this.onePokemon.stats[0].base_stat})
+
+              //Assegno le statistiche all'oggetto onePokemon eliminati gli object assign
+              this.onePokemon = {
+                hp: this.onePokemon.stats[0].base_stat,
+                attack: this.onePokemon.stats[1].base_stat,
+                defense: this.onePokemon.stats[2].base_stat,
+                specialAttack: this.onePokemon.stats[3].base_stat,
+                specialDefense: this.onePokemon.stats[4].base_stat,
+                speed: this.onePokemon.stats[5].base_stat,
+                typo: this.onePokemon.types[0].type.name,
+              }
+              /*Object.assign(this.onePokemon, {hp: this.onePokemon.stats[0].base_stat})
               Object.assign(this.onePokemon, {attack: this.onePokemon.stats[1].base_stat})
               Object.assign(this.onePokemon, {defense: this.onePokemon.stats[2].base_stat})
               Object.assign(this.onePokemon, {specialAttack: this.onePokemon.stats[3].base_stat})
               Object.assign(this.onePokemon, {specialDefense: this.onePokemon.stats[4].base_stat})
               Object.assign(this.onePokemon, {speed: this.onePokemon.stats[5].base_stat})
               //Assegno il tipo di pokemon all'oggetto onePokemon
-              Object.assign(this.onePokemon, {typo: this.onePokemon.types[0].type.name})
+              Object.assign(this.onePokemon, {typo: this.onePokemon.types[0].type.name})*/
             })
           )
         }
         this.isLoading = false;
         pokemonCompleteList()
-        console.log("Pokemon")
       },
       error: (err) => { //Gestisco gli errori del subscribe
         console.error(err.message)
         this.errormessage = err.message
       },
-      complete: () => {}
+      complete: () => {
+      }
     })
   }
 }
