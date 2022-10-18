057b8923eeba43221098ac8e4111ef50bdf7bb94
GiacomoMansi <giacomomansi@hotmail.com>
2022-10-18 17:25:42 +0200

    inserito il service pokemon nel componente

--- src/app/components/pokemon-cards/pokemon-cards.component.ts
+++ src/app/components/pokemon-cards/pokemon-cards.component.ts
@@ -4,6 +4,7 @@ import {HttpClient, } from "@angular/common/http";
 
 
 import {firstValueFrom} from "rxjs";
+import {PokemonService} from "../../services/pokemon.service";
 
 export interface Pokemon {
   name: string;
@@ -29,7 +30,7 @@ export class PokemonApi {
 export class PokemonSearchComponent implements OnInit {
 
 
-  constructor(public http: HttpClient,) {
+  constructor(public http: HttpClient, public _pokemonService: PokemonService) {
   }
 
   public pokemonApi: any = []; // Array contenente la chiamata all'api
@@ -41,42 +42,42 @@ export class PokemonSearchComponent implements OnInit {
 
 
   async ngOnInit() {
-    await this.getPokemon()
+    await this._pokemonService.getPokemon()
   }
 
-  async getPokemon() { //To Promise invertito in subscribe
-    this.http.get("https://pokeapi.co/api/v2/pokemon/?offset=0&limit=500").subscribe({
-      next: (result: any,) => {
-        this.pokemonApi = result.results
-        const pokemonCompleteList = async (): Promise<any> => {
-          await Promise.all(
-            this.pokemonApi.map(async (pokemon: any): Promise<any> => {
-              this.onePokemon = await firstValueFrom(this.http.get(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`))
-                .catch(err =>
-                  this.errormessage = err.message)//Gestisco gli errori
-              this.pokemon.push(this.onePokemon) //Inserisco il singolo pokemon nell'array pokemon
-              //Assegno le statistiche all'oggetto onePokemon
-              Object.assign(this.onePokemon, {hp: this.onePokemon.stats[0].base_stat})
-              Object.assign(this.onePokemon, {attack: this.onePokemon.stats[1].base_stat})
-              Object.assign(this.onePokemon, {defense: this.onePokemon.stats[2].base_stat})
-              Object.assign(this.onePokemon, {specialAttack: this.onePokemon.stats[3].base_stat})
-              Object.assign(this.onePokemon, {specialDefense: this.onePokemon.stats[4].base_stat})
-              Object.assign(this.onePokemon, {speed: this.onePokemon.stats[5].base_stat})
-              //Assegno il tipo di pokemon all'oggetto onePokemon
-              Object.assign(this.onePokemon, {typo: this.onePokemon.types[0].type.name})
-            })
-          )
-        }
-        this.isLoading = false;
-        pokemonCompleteList()
-      },
-      error: (err) => { //Gestisco gli errori del subscribe
-        console.error(err.message)
-        this.errormessage = err.message
-      },
-      complete: () => {}
-    })
-  }
+  // async getPokemon() { //To Promise invertito in subscribe
+  //   this.http.get("https://pokeapi.co/api/v2/pokemon/?offset=0&limit=500").subscribe({
+  //     next: (result: any,) => {
+  //       this.pokemonApi = result.results
+  //       const pokemonCompleteList = async (): Promise<any> => {
+  //         await Promise.all(
+  //           this.pokemonApi.map(async (pokemon: any): Promise<any> => {
+  //             this.onePokemon = await firstValueFrom(this.http.get(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`))
+  //               .catch(err =>
+  //                 this.errormessage = err.message)//Gestisco gli errori
+  //             this.pokemon.push(this.onePokemon) //Inserisco il singolo pokemon nell'array pokemon
+  //             //Assegno le statistiche all'oggetto onePokemon
+  //             Object.assign(this.onePokemon, {hp: this.onePokemon.stats[0].base_stat})
+  //             Object.assign(this.onePokemon, {attack: this.onePokemon.stats[1].base_stat})
+  //             Object.assign(this.onePokemon, {defense: this.onePokemon.stats[2].base_stat})
+  //             Object.assign(this.onePokemon, {specialAttack: this.onePokemon.stats[3].base_stat})
+  //             Object.assign(this.onePokemon, {specialDefense: this.onePokemon.stats[4].base_stat})
+  //             Object.assign(this.onePokemon, {speed: this.onePokemon.stats[5].base_stat})
+  //             //Assegno il tipo di pokemon all'oggetto onePokemon
+  //             Object.assign(this.onePokemon, {typo: this.onePokemon.types[0].type.name})
+  //           })
+  //         )
+  //       }
+  //       this.isLoading = false;
+  //       pokemonCompleteList()
+  //     },
+  //     error: (err) => { //Gestisco gli errori del subscribe
+  //       console.error(err.message)
+  //       this.errormessage = err.message
+  //     },
+  //     complete: () => {}
+  //   })
+  // }
 
 
 
